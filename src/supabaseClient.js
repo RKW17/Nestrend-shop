fa641c196a0b587acad0061f8268bbd758a27741
Richard Waliba <walibarichard9@gmail.com>
2026-01-23 21:32:34 +0000

    syncing fixes

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,5 @@
-import { createClient } from '@supabase/supabase-client'
 
-// Use your Project URL here
-const supabaseUrl = 'https://tqifawxmxsxtcauuzvwt.supabase.co' 
-
-// Use the LONG PUBLIC key you just sent (the first one)
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxaWZhd3hteHN0Y2F1dXp2d3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwODY2NTMsImV4cCI6MjA4MzY2MjY1M30.Wo5e5gdPA45FaBd8g0DFTspxky2qt3nhYFtXob3eN0U'
-
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://tqifawxmxstcauuzvwte.supabase.co'
+const supabaseKey = process.env.sb_publishable_UhA0rRAcW4IvuwzueIDOtQ_xciyqqpr
 export const supabase = createClient(supabaseUrl, supabaseKey)
