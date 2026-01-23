27db4e549f8dd842efcf75ebe6786ea481c1a88c
Richard Waliba <walibarichard9@gmail.com>
2026-01-23 14:43:12 +0000

    Fixing fetch error and updating client

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-client'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+// Use your Project URL here
+const supabaseUrl = 'https://tqifawxmxsxtcauuzvwt.supabase.co' 
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Use the LONG PUBLIC key you just sent (the first one)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxaWZhd3hteHN0Y2F1dXp2d3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwODY2NTMsImV4cCI6MjA4MzY2MjY1M30.Wo5e5gdPA45FaBd8g0DFTspxky2qt3nhYFtXob3eN0U'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
